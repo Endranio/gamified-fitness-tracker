ccd85e2c3c88598fef64a527aac3bed3edde3b5c
endranio <endranio576@gmail.com>
2025-07-25 14:42:35 +0700

    fix profile

--- backend/services/profile-service.go
+++ backend/services/profile-service.go
@@ -15,41 +15,26 @@ func (s *ProfileService) GetProfile(userID uint) (int, int, int, error) {
 
 	xp := user.XP
 	level := calculateLevel(xp)
-	progress := calculateProgress(xp, level)
+	progress := calculateProgress(xp)
 
 	return xp, level, progress, nil
 }
 
 
 func calculateLevel(xp int) int {
-	level := 1
-	threshold := 500
-
-	for xp >= threshold {
-		level++
-		threshold = nextThreshold(level)
-	}
-	return level
+    return (xp / 500) 
 }
 
-func nextThreshold(level int) int {
-	if level == 1 {
-		return 500
-	}
-	return (level - 1) * 1000
+
+
+
+func calculateProgress(xp int) int {
+    xpInCurrentLevel := xp % 500         
+    progress := (xpInCurrentLevel * 100) / 500 
+
+    if progress > 100 {
+        return 100
+    }
+    return progress
 }
 
-func calculateProgress(xp, level int) int {
-	if level == 1 {
-		return xp * 100 / 500
-	}
-
-	currLevelXP := nextThreshold(level - 1)
-	nextLevelXP := nextThreshold(level)
-
-	progress := (xp - currLevelXP) * 100 / (nextLevelXP - currLevelXP)
-	if progress > 100 {
-		return 100
-	}
-	return progress
-}
